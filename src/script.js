02c0f6841387887a9886f6b19ae08e2b7dc03bb8
Baran Khatibi <barankhatib99@gmail.com>
2025-01-05 03:21:52 +0330

    added search engine functionality

--- src/script.js
+++ src/script.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector(".weather-app-degree");
+  let temperature = response.data.daily["0"].temperature.day;
+  let cityElement = document.querySelector("#city");
+  
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "243tde0baafa0604eoa0cf81b23fedf1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchHandler(event) {
   event.preventDefault();
   let inputElement = document.querySelector("#form-input-text");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = inputElement.value;
+  
+  searchCity(inputElement.value);
 }
 
 let formElement = document.querySelector("#search-form-id");

053b3da0affe22e23931c202d141671e00ea5fd5
Baran Khatibi <barankhatib99@gmail.com>
2025-01-07 15:24:10 +0330

    fixed the API for current weather

--- src/script.js
+++ src/script.js
@@ -1,25 +1,26 @@
 function refreshWeather(response) {
+
   let tempElement = document.querySelector(".weather-app-degree");
-  let temperature = response.data.daily["0"].temperature.day;
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.daily["0"].time * 1000);
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  console.log(response.data.daily["0"].condition.icon_url);
 
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temperature);
-  descriptionElement.innerHTML = response.data.daily["0"].condition.description;
-  humidityElement.innerHTML = `${response.data.daily["0"].temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.daily["0"].wind.speed}km/h`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `              <img
-                src="${response.data.daily["0"].condition.icon_url}"
+                src="${response.data.condition.icon_url}"
                 class="weather-app-icon"
+                alt="${response.data.condition.icon}"
               />`;
 }
 
@@ -48,7 +49,8 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "243tde0baafa0604eoa0cf81b23fedf1";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  // https://api.shecodes.io/weather/v1/current?query={query}&key={key}
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(refreshWeather);
 }
